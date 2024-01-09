8947bbd763cf1db6d2a0c3766a8e9567f8044272
AdriaCParra <Costaadria524@gmail.com>
2024-01-09 15:15:56 +0100

    FINISH feedMonkeys done!

---
+++ 00-imperative/js/feedMonkeys.test.js
@@ -0,0 +1,20 @@
+import { feedMonkeys } from "./feedMonkeys";
+
+describe("Given the feedMonkeys function", () => {
+  test('When it receives 🍌, Then it should return ["🐒 🍌", "🦍 🍌", "🦧 🍌"]', () => {
+    const fruit = "🍌";
+
+    const result = feedMonkeys(fruit);
+
+    expect(result).toBeDefined();
+    expect(result).toEqual(["🐒 🍌", "🦍 🍌", "🦧 🍌"]);
+  });
+  test('When it receives 🍎, Then it should return ["🐒 🍎", "🦍 🍎", "🦧 🍎"]', () => {
+    const fruit = "🍎";
+
+    const result = feedMonkeys(fruit);
+
+    expect(result).toBeDefined();
+    expect(result).toEqual(["🐒 🍎", "🦍 🍎", "🦧 🍎"]);
+  });
+});
