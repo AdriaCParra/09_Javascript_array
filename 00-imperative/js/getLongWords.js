0e1b414af91aff561147f7f82120f84b0dfacc2c
AdriaCParra <Costaadria524@gmail.com>
2024-01-09 16:38:56 +0100

    Refactor and finish

--- 00-imperative/js/getLongWords.js
+++ 00-imperative/js/getLongWords.js
@@ -8,3 +8,16 @@
 
 const LANGUAGES = ["Java", "C++", "JavaScript", "C#", "TypeScript"];
 const BEATLES = ["John", "George", "Paul", "Ringo"];
+
+export const getLongWords = (words) => {
+  const wordsLongerThanFive = [];
+  const wordsLength = words.length;
+
+  for (let index = 0; index < wordsLength; index++) {
+    const word = words[index];
+    const wordLength = word.length;
+
+    if (wordLength >= 5) wordsLongerThanFive.push(word);
+  }
+  return wordsLongerThanFive;
+};
