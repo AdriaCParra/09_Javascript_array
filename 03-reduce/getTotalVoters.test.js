9e50a97764bf740ee5aafb0ec92a627f810e9244
AdriaCParra <Costaadria524@gmail.com>
2024-01-22 19:33:33 +0100

    FINISH getTotalVoters done!

--- 03-reduce/getTotalVoters.test.js
+++ 03-reduce/getTotalVoters.test.js
@@ -1,5 +1,25 @@
-describe("", () => {
-  test("", () => {
-    expect(true).toBe(true);
+import { getTotalVoters } from "./getTotalVoters";
+
+describe("Given getTotalVoters", () => {
+  test("When it recieves VOTERS, it should return a count of how many of them voted(Have the voted field true)", () => {
+    const VOTERS = [
+      { name: "Bob", age: 30, voted: true },
+      { name: "Jake", age: 32, voted: true },
+      { name: "Kate", age: 25, voted: false },
+      { name: "Sam", age: 20, voted: false },
+      { name: "Phil", age: 21, voted: true },
+      { name: "Ed", age: 55, voted: true },
+      { name: "Tami", age: 54, voted: true },
+      { name: "Mary", age: 31, voted: false },
+      { name: "Becky", age: 43, voted: false },
+      { name: "Joey", age: 41, voted: true },
+      { name: "Jeff", age: 30, voted: true },
+      { name: "Zack", age: 19, voted: false },
+    ];
+
+    const result = getTotalVoters(VOTERS);
+
+    expect(result).toBeDefined();
+    expect(result).toBe(7);
   });
 });
