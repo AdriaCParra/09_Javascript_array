0833b2e6d47263508124d3ed11b88206d151079b
AdriaCParra <Costaadria524@gmail.com>
2024-01-22 20:01:57 +0100

    FINISH calculateShopping done!

--- 03-reduce/calculateShopping.test.js
+++ 03-reduce/calculateShopping.test.js
@@ -1,5 +1,19 @@
-describe("", () => {
-  test("", () => {
-    expect(true).toBe(true);
+import { calculateShopping } from "./calculateShopping";
+
+describe("Given calculateShopping", () => {
+  test("When called with WISHES, should return 227005", () => {
+    const WISHES = [
+      { title: "Tesla Model S", price: 90000 },
+      { title: "4 carat diamond ring", price: 45000 },
+      { title: "Fancy hacky Sack", price: 5 },
+      { title: "Gold fidgit spinner", price: 2000 },
+      { title: "A second Tesla Model S", price: 90000 },
+    ];
+    const expected = 227005;
+
+    const result = calculateShopping(WISHES);
+
+    expect(result).toBeDefined();
+    expect(result).toBe(expected);
   });
 });
